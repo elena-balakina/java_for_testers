8aa7ba4bbce0a9232443360323ca1f5d7e3e942a
Elena Balakina <e.p.balakina@mail.ru>
2020-03-13 15:45:49 +0700

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,26 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "Elena";
+        double length = 5;
+        double a = 5;
+        double b = 7;
+
+        hello(name);
+        System.out.println("Площадь квадрата со стороной " + length + " = " + area(length));
+        System.out.printf("Площадь прямоугольника со сторонами %.2f и %.2f = %.2f", a, b, area(a, b));
     }
 }
