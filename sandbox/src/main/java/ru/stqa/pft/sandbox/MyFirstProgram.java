7bc453c182df5fcf3e9069c9bee401a2998599a5
Elena Balakina <e.p.balakina@mail.ru>
2020-03-13 16:17:14 +0700

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,22 +5,29 @@ public class MyFirstProgram {
         System.out.println("Hello, " + name + "!");
     }
 
-    public static double area(double l) {
-        return l * l;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
     }
 
     public static void main(String[] args) {
         String name = "Elena";
-        double length = 5;
-        double a = 5;
-        double b = 7;
-
         hello(name);
-        System.out.println("Площадь квадрата со стороной " + length + " = " + area(length));
-        System.out.printf("Площадь прямоугольника со сторонами %.2f и %.2f = %.2f", a, b, area(a, b));
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(5, 7);
+        System.out.printf("Площадь прямоугольника со сторонами %.2f и %.2f = %.2f%n", r.a, r.b, r.area());
+
+        Point p1 = new Point(1, 2.5);
+        Point p2 = new Point(5, 7.5);
+
+        System.out.println();
+        System.out.println("Результат с помощью функции:");
+        System.out.printf("Расстояние между точками (%.2f; %.2f) и (%.2f; %.2f) = %.2f%n", p1.x, p1.y, p2.x, p2.y, distance(p1, p2));
+
+        System.out.println();
+        System.out.println("Результат с помощью метода класса:");
+        System.out.printf("Расстояние между точками (%.2f; %.2f) и (%.2f; %.2f) = %.2f%n", p1.x, p1.y, p2.x, p2.y, p1.distance(p1, p2));
     }
 }
