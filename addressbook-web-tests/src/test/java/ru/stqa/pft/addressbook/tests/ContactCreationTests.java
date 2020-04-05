c1daec2489445ad43dc5efc0e28fe300c55dad1b
Elena Balakina <e.p.balakina@mail.ru>
2020-04-05 17:47:15 +0700

    Задание №9. промежуточный этап - 6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.HashSet;
 import java.util.List;
@@ -14,21 +13,13 @@ public class ContactCreationTests extends TestBase {
     public void testContactCreation() throws Exception {
         List<ContactData> before = app.getContactHelper().getContactList();
 
-        ContactData contact = new ContactData("Roman", "Popkov", "New York", "+79701111122", "mail2@gmail.com", "test1");
+        ContactData contact = new ContactData("Lada", "Yakovleva", "Saratov", "+79701111122", "mail2@gmail.com", "test1");
         app.getContactHelper().createContact(contact, true);
 
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-
-        for (ContactData c: after){
-            if (c.getId()>max){
-                max = c.getId();
-            }
-        }
-
-        contact.setId(max);
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
