91048e2cc6c9c39e8ab14dab1fa35fdf2a26e3a1
Elena Balakina <e.p.balakina@mail.ru>
2020-04-19 16:57:06 +0700

    Задание №12. промежуточный этап - 2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -11,15 +14,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
 
-        List<ContactData> contact = generateContacts(count);
-        saveAsCSV(contact, file);
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    //  @Parameter(names = "-d", description = "Data format")
+    //public String format;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+
+        generator.run();
     }
 
-    private static void saveAsCSV(List<ContactData> contacts, File file) throws IOException {
+    private void run() throws IOException {
+        List<ContactData> contact = generateContacts(count);
+        saveAsCSV(contact, new File(file));
+
+        /*if (format.equals("csv")) {
+            saveAsCSV(groups, new File(file));
+        } else if (format.equals("xml")) {
+            SaveAsXML(groups, new File(file));
+        } else if (format.equals("json")) {
+            SaveAsJson(groups, new File(file));
+        } else {
+            System.out.println("Unrecognized format");
+        }*/
+    }
+
+    private void saveAsCSV(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
 
