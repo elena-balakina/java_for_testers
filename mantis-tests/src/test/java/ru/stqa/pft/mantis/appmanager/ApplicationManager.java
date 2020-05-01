bda6c4aa7f1546ca9b1c28c735d7fe48534f4f5d
Elena Balakina <e.p.balakina@mail.ru>
2020-05-02 00:36:35 +0700

    Лекция 8.4. Ленивая инициализация помощников

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -11,11 +11,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.MatchResult;
 
 public class ApplicationManager {
     private final Properties properties;
     private String browser;
-    public WebDriver driver; // webdrivers лежат в папке C:\Windows\System32
+    private WebDriver driver; // webdrivers лежат в папке C:\Windows\System32
+    private RegistrationHelper registrationHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -25,29 +27,42 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-        if (browser.equals(BrowserType.CHROME)) {
-            driver = new ChromeDriver();
-        } else if (browser.equals(BrowserType.FIREFOX)) {
-            driver = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            driver = new InternetExplorerDriver();
-        }
-
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        driver.get(properties.getProperty("web.baseUrl"));       //driver.get("http://localhost/mantisbt-2.24.0/");
     }
 
     public void stop() {
-        //navigationHelper.clickLink("Logout");
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
-    public HTTPSession newSession(){
+    public HTTPSession newSession() {
         return new HTTPSession(this);
     }
 
     public String getProperty(String key) {
         return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (driver == null) {
+            if (browser.equals(BrowserType.CHROME)) {
+                driver = new ChromeDriver();
+            } else if (browser.equals(BrowserType.FIREFOX)) {
+                driver = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                driver = new InternetExplorerDriver();
+            }
+
+            driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            driver.get(properties.getProperty("web.baseUrl"));       //driver.get("http://localhost/mantisbt-2.24.0/");
+        }
+        return driver;
     }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
@@ -12,7 +12,7 @@ public class LoginTests extends TestBase {
     @Test
     public void testLogin() throws IOException {
         HTTPSession session = app.newSession();
-        
+
         assertTrue(session.login("administrator", "root"));
         assertTrue(session.isLoggedInAs("administrator"));
     }
