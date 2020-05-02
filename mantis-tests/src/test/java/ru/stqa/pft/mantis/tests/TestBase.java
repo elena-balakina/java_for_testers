b42fc157b327acaafb37e6152454d0f4c952b309
Elena Balakina <e.p.balakina@mail.ru>
2020-05-02 20:46:19 +0700

    Лекция 8.5. Протокол FTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,8 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+
 public class TestBase {
 
     protected static final ApplicationManager app
@@ -18,12 +20,21 @@ public class TestBase {
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
+        //app.ftp().upload(new File("src/test/resources/config_inc.php"),
+        //"config_inc.php", "config_inc.php.bak");
+
+        app.ftp().upload(new File("src/test/resources/config_inc.php"),
+                "config/config_inc.php", "config/config_inc.php.bak");
     }
 
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
+        //app.ftp().restore("config_inc.php.bak","config_inc.php");
+        app.ftp().restore("config/config_inc.php.bak", "config/config_inc.php");
         app.stop();
     }
 
 
 }
+
+// $g_crypto_master_salt     = 'UEYla15Z1dYa1H0K7Ymgx7S8RuMHdhJ8sWrO6UMvngw=';
