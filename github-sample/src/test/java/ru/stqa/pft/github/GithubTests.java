6c20880c05c9dde1dc119de183ef6b0021c6d3fe
Elena Balakina <e.p.balakina@mail.ru>
2020-05-15 22:33:49 +0700

    Лекция 9.6. REST: взаимодействие с GitHub

--- github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -1,13 +1,21 @@
 package ru.stqa.pft.github;
 
-import com.jcabi.github.Github;
-import com.jcabi.github.RtGithub;
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class GithubTests {
 
     @Test
-    public void testCommits(){
-        Github github = new RtGithub(".. your OAuth token ..");
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("11f4bc48759bbdedb4693247ef1feeb4487ed560");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("elena-balakina",
+                "java_for_testers")).commits();
+
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
     }
 }
